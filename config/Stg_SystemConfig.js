103ff5424fa33c47e2d5b148f4c40ca87573a4d5
lingxue <info@myxxjs.com>
2017-04-06 15:32:24 +0800

    update log config

--- config/Stg_SystemConfig.js
+++ config/Stg_SystemConfig.js
@@ -1,19 +1,17 @@
 
 var logLevel = 'DEBUG';
 var loggerConfig = {
-    level : 'debug',
-    config : {
-        appenders: [
-            { type: 'console' },
-            {
-                "type": "file",
-                "filename": "../stage/log_file.html",
-                "maxLogSize":20480,
-                "backups": 1
-            }
-        ]
-    }
+    appenders: [
+        { type: 'console' },
+        {
+            "type": "file",
+            "filename": "../logs/logistic.log",
+            "maxLogSize": 2048000,
+            "backups": 10
+        }
+    ]
 }
+
 
 var mongoConfig = {
     connect : 'mongodb://localhost:27017/log_file'
