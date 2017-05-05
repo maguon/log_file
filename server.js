b1d73f66f7ec20843290d3e75675bde67b9dce2a
lingxue <info@myxxjs.com>
2017-05-05 11:39:08 +0800

    update log config

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ function createServer() {
         ip: true
     }));
     restify.CORS.ALLOW_HEADERS.push('auth-token');
-    restify.CORS.ALLOW_HEADERS.push('client-id');
+    restify.CORS.ALLOW_HEADERS.push('user-name');
+    restify.CORS.ALLOW_HEADERS.push('user-type');
+    restify.CORS.ALLOW_HEADERS.push('user-id');
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Origin");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Credentials");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Methods","GET");
